6a75192425f0db9c5292429405b0ec1158ba67be
ari889 <arijitbanarjee889@gmail.com>
2026-02-25 06:46:56 +0600

    Change system config
    1. Apply throttling
    2. Use helmet
    3. Add csrf protection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import {
+  ThrottlerGuard,
+  ThrottlerModule,
+  ThrottlerModuleOptions,
+} from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -10,9 +16,38 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
       envFilePath: `.${process.env.NODE_ENV || 'development'}.env`,
     }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): ThrottlerModuleOptions => {
+        console.log(
+          'THROTTLE_TTL at startup:',
+          configService.get('THROTTLE_TTL'),
+        );
+        console.log(
+          'THROTTLE_LIMIT at startup:',
+          configService.get('THROTTLE_LIMIT'),
+        );
+
+        return {
+          throttlers: [
+            {
+              ttl: Number(configService.get('THROTTLE_TTL') || 10),
+              limit: Number(configService.get('THROTTLE_LIMIT') || 1),
+            },
+          ],
+        };
+      },
+    }),
     UsersModule,
     PrismaModule,
     AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,61 @@ import {
 import { ConfigService } from '@nestjs/config';
 import { setupSwagger } from './swagger';
 import { VersioningType } from '@nestjs/common';
+import fastifyHelmet from '@fastify/helmet';
+import fastifyCsrfProtection from '@fastify/csrf-protection';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({
       logger: process.env.NODE_ENV === 'development',
+      trustProxy: true,
     }),
   );
 
+  /**
+   * apply cors
+   */
+  app.enableCors({
+    origin: [
+      process.env.FRONTEND_URL as string,
+      process.env.SWAGGER_URL as string,
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+  });
+
+  /**
+   * apply helmet
+   */
+  await app.register(fastifyHelmet);
+
+  /**
+   * csrf protection
+   */
+  await app.register(fastifyCsrfProtection);
+
+  /**
+   * set global prefix as /api/admin
+   */
   app.setGlobalPrefix('api/admin');
 
+  /**
+   * enable api versioning
+   */
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
+  /**
+   * get service config
+   */
   const configService = app.get(ConfigService);
 
+  /**
+   * initialize swagger
+   */
   setupSwagger(app, configService);
 
   await app.listen(process.env.PORT ?? 3000);
